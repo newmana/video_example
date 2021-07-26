a14398c0a33cbb0dc2625a2c87e96a80223a83c3
Andrew Newman <andrewfnewman@gmail.com>
2021-07-26 20:41:04 +1000

    Another test.

--- tests/project_tests.py
+++ tests/project_tests.py
@@ -5,5 +5,7 @@ from main import *
 class TestProjectCode(unittest.TestCase):
     def test_numbers(self):
         assert(add_numbers(1, 4) == 5)
+
+    def test_negative_add(self):
         assert (add_numbers(-1, 4) == 3)
 
